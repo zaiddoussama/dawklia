9c387d40172b01db9b4b0d5f5143fdc5c06bd0f2
zaiddoussama <zaiddoussama@gmail.com>
2023-10-23 22:38:52 +0100

    initial commit

---
+++ src/main/java/com/pushapp/dawklia/DawkliaApplication.java
@@ -0,0 +1,13 @@
+package com.pushapp.dawklia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawkliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawkliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pushapp/dawklia/DawkliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pushapp.dawklia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawkliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
